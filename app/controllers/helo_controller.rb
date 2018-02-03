5bcec3ab694accc24086686dc49733bee3d3c913
HideakiTouhara <hideee6723@gmail.com>
2018-02-04 01:06:23 +0900

    Use redirect

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -1,4 +1,14 @@
 class HeloController < ApplicationController
   def index
+    if params['msg'] != nil then
+      @title = params['msg']
+    else
+      @title = 'index'
+    end
+    @msg = 'this is redirect sample'
+  end
+
+  def other
+    redirect_to action: :index, params: {'msg': 'from other page'}
   end
 end
