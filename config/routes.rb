23c2ea5b667314609790a29ca1958ca589f8c63b
HideakiTouhara <hideee6723@gmail.com>
2018-03-28 13:28:16 +0900

    Create message app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+
+  get 'messages/how'
+
+  get 'messages/add'
+
+  get 'messages/edit'
+
   get 'people/index'
   get 'people', to: 'people#index'
 
@@ -15,6 +23,19 @@ Rails.application.routes.draw do
   post 'people/find'
 
   get 'people/:id', to: 'people#show'
+
+  get 'messages/index'
+  get 'messages', to: 'messages#index'
+
+  get 'messages/add'
+  post 'messages/add', to: 'messages#create'
+
+  get 'messages/edit/:id', to: 'messages#edit'
+  patch 'messages/edit/:id', to: 'messages#update'
+
+  get 'messages/delete/:id', to: 'messages#delete'
+
+  get 'messages/:id', to: 'messages#show'
 
   get 'cards/index'
   get 'cards', to: 'cards#index'
