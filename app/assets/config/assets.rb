83abcdc73ce50eeca6a8fc5ee2285292ed924be0
HideakiTouhara <hideee6723@gmail.com>
2018-04-05 20:11:19 +0900

    stylesheet

---
+++ app/assets/config/assets.rb
@@ -0,0 +1,13 @@
+Rails.application.config.assets.precompile += %w( blogconfigs.css )
+Rails.application.config.assets.precompile += %w( blogconfigs.js )
+
+Rails.application.config.assets.precompile += %w( bloggenres.css )
+Rails.application.config.assets.precompile += %w( bloggenres.js )
+
+Rails.application.config.assets.precompile += %w( blogposts.css )
+Rails.application.config.assets.precompile += %w( blogposts.js )
+
+Rails.application.config.assets.precompile += %w( blogs.css )
+Rails.application.config.assets.precompile += %w( blogs.js )
+
+Rails.application.config.assets.precompile += %w( gray.css )
