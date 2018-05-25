8a11dfc0374268326eddf8a6007be3f7de275485
HideakiTouhara <hideee6723@gmail.com>
2018-05-25 13:52:24 +0900

    scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180521122828) do
+ActiveRecord::Schema.define(version: 20180525045150) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -62,12 +62,33 @@ ActiveRecord::Schema.define(version: 20180521122828) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "board_messages", force: :cascade do |t|
+    t.text "content"
+    t.integer "board_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "board_users", force: :cascade do |t|
+    t.text "nickname"
+    t.integer "account_id"
+    t.text "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "cards", force: :cascade do |t|
     t.text "title"
     t.text "author"
     t.integer "price"
     t.text "publisher"
     t.text "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "data_pages", force: :cascade do |t|
+    t.text "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
