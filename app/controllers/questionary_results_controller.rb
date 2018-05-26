92e98e56ef45298f1bae1b7cfc49dfa2eef55947
HideakiTouhara <hideee6723@gmail.com>
2018-05-26 18:53:54 +0900

    last

--- app/controllers/questionary_results_controller.rb
+++ app/controllers/questionary_results_controller.rb
@@ -1,15 +1,38 @@
 class QuestionaryResultsController < ApplicationController
-  before_action :set_questionary_result, only: [:show, :edit, :update, :destroy]
+  #before_action :set_questionary_result, only: [:show, :edit, :update, :destroy]
+  layout 'questionaries'
 
   # GET /questionary_results
   # GET /questionary_results.json
   def index
-    @questionary_results = QuestionaryResult.all
+    @questionaries = Questionary.all
   end
 
   # GET /questionary_results/1
   # GET /questionary_results/1.json
   def show
+    @questionary = Questionary.find params[:id]
+    @questionary_results = QuestionaryResult.where('questionary_id = ?',params[:id])
+  end
+
+  def calc
+    @questionary = Questionary.find params[:id]
+    results = QuestionaryResult.where('questionary_id = ?',params[:id])
+    @calc = {}
+    results.each do |result|
+      data = result.result.split ','
+      data.each do |value|
+        keyval = value.split ':'
+        ky = keyval[0].to_s
+        vl = keyval[1].to_i
+        if ky != 'question_id' then
+          if @calc[ky] == nil then
+            @calc[ky] = []
+          end
+          @calc[ky][vl] = @calc[ky][vl] == nil ? 1 : @calc[ky][vl].to_i + 1
+        end
+      end
+    end
   end
 
   # GET /questionary_results/new
@@ -62,13 +85,13 @@ class QuestionaryResultsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_questionary_result
-      @questionary_result = QuestionaryResult.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_questionary_result
+    @questionary_result = QuestionaryResult.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def questionary_result_params
-      params.require(:questionary_result).permit(:questionary_id, :result)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def questionary_result_params
+    params.require(:questionary_result).permit(:questionary_item, :result)
+  end
 end
